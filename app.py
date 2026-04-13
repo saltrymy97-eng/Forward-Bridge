7a2673e88e6502bcf86e4cae86c9a8484f0545bb
سالم التريمي <254166838+saltrymy97-eng@users.noreply.github.com>
2026-04-13 09:26:48 +0300

    Refactor JSON loading for quiz data

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def start_quiz(methodology_name):
             json_end = response.rfind('}') + 1
             if json_start != -1 and json_end != 0:
                 json_str = response[json_start:json_end]
-                quiz_data = json.loads(json_str)
+                quiz_data, _ = json.JSONDecoder().raw_decode(json_str)
                 st.session_state.quiz_questions = quiz_data['questions']
                 st.session_state.quiz_current = 0
                 st.session_state.quiz_answers = [None] * len(quiz_data['questions'])
